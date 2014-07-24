796ad4fa0d36ecda70d55d3061660d5f756b9eaa
Ryan D'souza <dsouzarc@gmail.com>
2014-07-24 09:47:31 -0400

    Solution to Problem 29

---
+++ Problem29.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+/** Written by Ryan D'souza
+  *  http://projecteuler.net/problem=29  */
+
+public class Problem29 {
+  public static void main(String[] args) {
+    HashMap<String, Double> theValues = new HashMap<String, Double>();
+    
+    for(int a = 2; a < 101; a++) {
+      for(int b = 2; b < 101; b++) {
+        double theProduct = Math.pow(a, b);
+        theValues.put(String.valueOf(theProduct), theProduct);
+      }
+    }
+    
+    System.out.println(theValues.size());
+  }
+}
+        
