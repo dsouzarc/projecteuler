5604303fa5d12f1e6c28cbf7ab0227d67f7c998d
Ryan D'souza <dsouzarc@gmail.com>
2014-07-24 09:46:35 -0400

    Added solution to Problem 48

---
+++ Problem48.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+/** Written by Ryan D'souza
+  *  http://projecteuler.net/problem=48  */
+public class Problem48 {
+  public static void main(String[] args) {
+   
+    BigInteger theSum = new BigInteger(String.valueOf("0"));
+    
+    for(int i = 1; i < 1001; i++)
+      theSum = theSum.add(new BigInteger(String.valueOf(i)).pow(i));
+    
+    final String theResult = theSum.toString();
+    System.out.println(theResult.substring(theResult.length()-10));
+  }
+}
